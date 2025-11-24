fe269f1864757d8a15d5ce2b7408bd3b6d03fb4b
Ei8 <9668999+ei8hundr8@users.noreply.github.com>
2025-11-25 02:21:34 +0800

    study: exercise - implement conditional operator

---
+++ Module03_EvaluateBoolean/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Module03_EvaluateBoolean
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int saleAmount = 1001;
+			//int discount = saleAmount > 1000 ? 100 : 50;
+			Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+		}
+	}
+}
