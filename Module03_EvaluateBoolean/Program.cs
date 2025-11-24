36877f325e707576d2bc9031ac85262838042d25
Ei8 <9668999+ei8hundr8@users.noreply.github.com>
2025-11-25 02:33:23 +0800

    study: exercise - coin toss

--- Module03_EvaluateBoolean/Program.cs
+++ Module03_EvaluateBoolean/Program.cs
@@ -4,9 +4,11 @@
 	{
 		static void Main(string[] args)
 		{
-			int saleAmount = 1001;
-			//int discount = saleAmount > 1000 ? 100 : 50;
-			Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+			Random random = new Random();
+			int coinToss = random.Next(1, 3);
+
+			//string coinResult = coinToss == 1 ? "Head" : "Tail";
+			Console.WriteLine($"You got {(coinToss == 1 ? "Head" : "Tail")}!");
 		}
 	}
 }
