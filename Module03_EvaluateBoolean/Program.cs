ee633661f775fe92d60891e9dc3597528eac338d
Ei8 <9668999+ei8hundr8@users.noreply.github.com>
2025-11-30 03:46:29 +0800

    study: module03 challenge project 01

--- Module03_EvaluateBoolean/Program.cs
+++ Module03_EvaluateBoolean/Program.cs
@@ -4,63 +4,58 @@
 	{
 		static void Main(string[] args)
 		{
-			/* Here are the rules for the battle game that you need to implement in your code project:
-			 * You must use either the do-while statement or the while statement as an outer game loop.
-			 * The hero and the monster start with 10 health points.
-			 * All attacks are a value between 1 and 10.
-			 * The hero attacks first.
-			 * Print the amount of health the monster lost and their remaining health.
-			 * If the monster's health is greater than 0, it can attack the hero.
-			 * Print the amount of health the hero lost and their remaining health.
-			 * Continue this sequence of attacking until either the monster's health or hero's health is zero or less.
-			 * Print the winner.
+			/* Code project 1 - write code that validates integer input
+			 * Here are the conditions that your first coding project must implement:
+			 * Your solution must include either a do-while or while iteration.
+			 * Before the iteration block: your solution must use a Console.WriteLine() statement to prompt the user for an integer value between 5 and 10.
+			 * Inside the iteration block:
+			 * Your solution must use a Console.ReadLine() statement to obtain input from the user.
+			 *		Your solution must ensure that the input is a valid representation of an integer.
+			 *		If the integer value isn't between 5 and 10, your code must use a Console.WriteLine() statement to prompt the user for an integer value between 5 and 10.
+			 *		Your solution must ensure that the integer value is between 5 and 10 before exiting the iteration.
+			 * Below (after) the iteration code block: your solution must use a Console.WriteLine() statement to inform the user that their input value has been accepted.
 			 * 
-			 * Expected output:
-			 * Monster was damaged and lost 1 health and now has 9 health.
-			 * Hero was damaged and lost 1 health and now has 9 health.
-			 * Monster was damaged and lost 7 health and now has 2 health.
-			 * Hero was damaged and lost 6 health and now has 3 health.
-			 * Monster was damaged and lost 9 health and now has -7 health.
-			 * Hero wins!
-			 * 
+			 * Example Output:
+			 * Enter an integer value between 5 and 10 
+			 * two
+			 * Sorry, you entered an invalid number, please try again
+			 * 2
+			 * You entered 2. Please enter a number between 5 and 10.
+			 * 7
+			 * Your input value (7) has been accepted.
 			 */
 
-			Random random = new Random();
+			string? input;
+			bool validEntry = false;
 
-			int hpPlayer = 250;
-			int hpEnemy = 250;
-			int damage;
+			Console.WriteLine("Enter a number between 5 and 10, Onii-chan!");
 
-			// Hero must attack first.
 			do
 			{
-				// Player can only attack when HP is above 0.
-				if (hpPlayer > 0)
-				{
-					damage = random.Next(1, hpEnemy + 1);
-					hpEnemy -= damage;
-					Console.WriteLine($"Hero attacks with {damage} damage! Monster HP is now {hpEnemy}!");
-				}
+				int intInput = 0;
+				bool validNumber = false;
 
-				// Enemy can only attack when HP is above 0.
-				if (hpEnemy > 0)
+				input = Console.ReadLine();
+
+				if (validNumber = int.TryParse(input, out intInput))
 				{
-					damage = random.Next(1, hpPlayer + 1);
-					hpPlayer -= damage;
-					Console.WriteLine($"Green Slime attacks with {damage} damage! Hero's HP is now {hpPlayer}!");
+					if (intInput >= 5 && intInput <= 10)
+					{
+						Console.WriteLine($"You entered the number {intInput}! Way to go, Onii-chan!");
+						validEntry = true;
+					}
+					else
+					{
+						Console.WriteLine($"{intInput} is not between 5 and 10, Onii-chan...");
+						continue;
+					}
+				}
+				else
+				{
+					Console.WriteLine($"\"{input}\" is not even a number, Onii-chan... Daijoubu?");
 				}
 			}
-			while (hpPlayer > 0 && hpEnemy > 0);
-
-			// End of battle when one of the character HP is negative.
-			if (hpPlayer <= 0)
-			{
-				Console.WriteLine("The hero fell...");
-			}
-			else
-			{
-				Console.WriteLine($"Hero wins with {hpPlayer}HP left!");
-			}
+			while (validEntry == false);
 
 		}
 	}
