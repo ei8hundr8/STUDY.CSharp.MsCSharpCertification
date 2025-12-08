2450754d1ee9d09f6c50336651eeec948135c786
Ei8 <9668999+ei8hundr8@users.noreply.github.com>
2025-12-09 00:06:33 +0800

    study: module 04 format alphanumerical data ex03

--- Module04_VariableData/Program.cs
+++ Module04_VariableData/Program.cs
@@ -4,18 +4,34 @@
 	{
 		static void Main(string[] args)
 		{
-			int invoiceNumber = 1201;
-			decimal productShare = 25.4568m;
-			decimal subtotal = 2750.00m;
-			decimal taxPercentage = .15825m;
-			decimal total = 3185.19m;
+			string input = "Pad this, Mi-chan!";
+			string input2 = "How about padding this?";
+			Console.WriteLine(input.PadLeft(25)); // the number inside Pad is the total number of characters for the output. Let's say the input is only 4 chars, then PadLeft(8) will add 4 extra chars to complete the total 8
+			Console.WriteLine(input2.PadRight(26));
 
-			Console.WriteLine($"Invoice Number: {invoiceNumber}");
-			Console.WriteLine($"Shares: {productShare:N3} Product");
-			Console.WriteLine($"	Sub Total: {subtotal:C}");
-			Console.WriteLine($"	Tax: {taxPercentage:P2}");
-			Console.WriteLine($"	Total Billed: {total:C}");
+			// Overloaded method
+			string input3 = "Special Padding Left!";
+			string input4 = "Special Padding RIght!";
+			Console.WriteLine(input3.PadLeft(30, '~'));
+			Console.WriteLine(input4.PadRight(31, '!'));
+			Console.WriteLine("");
 
+			// Padded strings
+			/*Suppose you work for a payment processing company that still supports legacy mainframe systems.
+			 * Often, those systems require data to be input in specific columns.
+			 * For example, store the Payment ID in columns 1 through 6, the payee's name in columns 7 through 30, 
+			 * and the Payment Amount in columns 31 through 40. Also, importantly, the Payment Amount is right-aligned.
+			 */
+			string paymentID = "769C";
+			string payeeName = "Mr. Stephen Ortega";
+			string paymentAmount = "$5000.00";
+
+			var formattedLine = paymentID.PadRight(6);
+			formattedLine += payeeName.PadRight(24);
+			formattedLine += paymentAmount.PadRight(10);
+
+			Console.WriteLine("1234567890123456789012345678901234567890");
+			Console.WriteLine(formattedLine);
 		}
 	}
 }
