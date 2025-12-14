fc8dd7917052bbc3bc08726bbb065529a787035c
Ei8 <9668999+ei8hundr8@users.noreply.github.com>
2025-12-14 19:38:12 +0800

    study: Module 05 Create Methods Ex01

--- Module05_CreateMethods/Program.cs
+++ Module05_CreateMethods/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void ThreeRoyalDice()
+{
+	Random random = new Random();
+	for (int i = 0; i < 3; i++)
+	{
+		Console.Write($"[{random.Next(1, 21)}] ");
+	}
+	Console.WriteLine();
+}
+
+Console.WriteLine("Rolling the dices...");
+
+string[] waitSymbols = [". ", ".. ", "... ", ".... "];
+for (int i = 0; i < 3; i++)
+{
+	foreach (string symbol in waitSymbols)
+	{
+		Console.Write($"\r{symbol}");
+		Thread.Sleep(250);
+	}
+	Console.Write($"\r{new String(' ', Console.BufferWidth)}");
+}
+ThreeRoyalDice();
+Console.WriteLine("\nThe Royal Dices have spoken...");
