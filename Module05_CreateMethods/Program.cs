034cabed8c4d6b4c9c2d3419878918be2b99cc52
Ei8 <9668999+ei8hundr8@users.noreply.github.com>
2025-12-27 15:31:43 +0800

    study: module 05 guided project 01

--- Module05_CreateMethods/Program.cs
+++ Module05_CreateMethods/Program.cs
@@ -1,87 +1,56 @@
-﻿/* Dice mini-game challenge
- * Your challenge is to design a mini-game. The game should select a target number that is a random number between one and five (inclusive). 
- * The player must roll a six-sided dice. 
- * To win, the player must roll a number greater than the target number. 
- * At the end of each round, the player should be asked if they would like to play again, and the game should continue or terminate accordingly.
- * 
- * In this challenge, you're given some starting code. You must determine what methods to create, their parameters, and their return types.
- * 
- * Code challenge: add methods to make the game playable 
- */
+﻿string[] pettingZoo = { "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises" };
 
-Random random = new Random();
-Console.WriteLine("Would you like to play, Traveler? [Y/N]");
-string? input = "";
-if (ShouldPlay(input))
+void PlanSchoolVisit(string schoolName, int groups = 6)
 {
-	PlayGame();
+	RandomizeAnimals();
+	string[,] group = AssignGroup(groups);
+	Console.WriteLine(schoolName);
+	PrintGroup(group);
+	Console.WriteLine();
 }
 
-void PlayGame()
+PlanSchoolVisit("Millennium Science School");
+PlanSchoolVisit("Trinity General School", 3);
+PlanSchoolVisit("Hyakkiyako Alliance Academy", 2);
+PlanSchoolVisit("Valykyrie Police School", 3);
+
+
+void RandomizeAnimals()
 {
-	var play = true;
+	Random random = new Random();
 
-	while (play)
+	for (int i = 0; i < pettingZoo.Length; i++)
 	{
-		var target = random.Next(1, 6);
-		var roll = random.Next(1, 7);
+		int r = random.Next(i, pettingZoo.Length);
 
-		Console.WriteLine($"\nRoll a number greater than {target} to win.");
-		Console.WriteLine($"You rolled a {roll}!");
-		Console.WriteLine(WinOrLose());
-		Console.WriteLine("\nPlay again? [Y/N]");
-
-		play = ShouldPlay(input);
-
-		string WinOrLose()
-		{
-			string result = "";
-			if (roll > target)
-			{
-				result = "You win, Traveler!";
-			}
-			else if (roll < target)
-			{
-				result = "Oh dear, you lose...";
-			}
-			else
-			{
-				result = "Oh my, it's a draw!";
-			}
-			return result;
-		}
+		string temp = pettingZoo[i];
+		pettingZoo[i] = pettingZoo[r];
+		pettingZoo[r] = temp;
 	}
 }
-
-bool ShouldPlay(string? input)
+string[,] AssignGroup(int groups = 6)
 {
-	bool valid = true;
-	do
+	string[,] result = new string[groups, pettingZoo.Length / groups];
+
+	int start = 0;
+	for (int i = 0; i < groups; i++)
 	{
-		input = Console.ReadLine();
-		if (input != null)
+		for (int j = 0; j < result.GetLength(1); j++)
 		{
-			input = input.Trim().ToLower();
-			switch (input)
-			{
-				case "y":
-				case "yes":
-				case "yep":
-				case "yeah":
-					return true;
-				case "n":
-				case "no":
-				case "nope":
-				case "nah":
-					Console.WriteLine("\nHope to see you again, dear Traveler.\nBye bye~");
-					return false;
-				default:
-					Console.WriteLine("Invalid selection, dear Traveler.");
-					valid = false;
-					break;
-			}
+			result[i, j] = pettingZoo[start++];
 		}
-		valid = false;
-	} while (valid == false);
-	return false;
+	}
+	return result;
+}
+void PrintGroup(string[,] group)
+{
+	for (int i = 0; i < group.GetLength(0); i++)
+	{
+		Console.Write($"Group {i + 1}: ");
+		for (int j = 0; j < group.GetLength(1); j++)
+		{
+			Console.Write($"{group[i, j]} ");
+		}
+		Console.WriteLine();
+	}
 }
